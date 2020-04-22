66be08360a15ff80307b5b30da9a199a65bbb3c4
4lon3ly0 <guillem0907rgmail.com>
2020-04-22 19:03:13 +0200

    error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 // settings
-app.set('port', 3000);
+app.set('port', process.env.PORT);
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
